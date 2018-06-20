1a719324d20fd6de365d2f2695ab3c7344eb07c6
charlie <charles@bbd.co.za>
2018-06-20 16:57:22 +0200

    Initial commit

---
+++ Keeper/Models/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace Keeper
+{
+    public class Item : ObservableObject
+    {
+        private string id = string.Empty;
+        private string text = string.Empty;
+        private string description = string.Empty;
+
+        [JsonIgnore]
+        public string Id
+        {
+            get { return id; }
+            set { SetProperty(ref id, value); }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { SetProperty(ref text, value); }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { SetProperty(ref description, value); }
+        }
+
+
+    }
+}
